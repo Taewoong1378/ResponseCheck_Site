3b02865b97428bb2540e7ea2c8bc252ad9984b31
TAEWOONG KANG <ktw2378@korea.ac.kr>
2021-07-27 16:53:20 +0900

    1차 완료

--- front/components/ResponseCheck.js
+++ front/components/ResponseCheck.js
@@ -18,15 +18,14 @@ const Wrapper = styled.div`
   font-weight: bold;
   user-select: none;
   color: ${props=> props.state==='ready' ? 'white' : 'black'};
-  background-color: ${props=> props.state === 'waiting' ? 'aqua' : props.state==='ready' ? 'red' : props.state==='finish' ? 'gray' : 'greenyellow'};
+  background-color: ${props=> props.state === 'waiting' ? 'aqua' : props.state ==='ready' ? 'red' : props.state ==='finish' ? 'gray' : 'greenyellow'};
 `;
 
-const TimeWrapper = styled.ul`
+const Ul = styled.ul`
   margin-top: 30px;
   position: absolute;
-  width: 100%;
   left: 50%;
-  transform: translate(-50%); 
+  transform: translate(-50%);
   font-weight: bold;
   line-height: 2.0em;
   font-size: 15px;
@@ -35,8 +34,8 @@ const TimeWrapper = styled.ul`
 `;
 
 const Li = styled.li`
-  font-size: 20px;
   text-align: center;
+  font-size: 20px;
   line-height: 2.0em;
 `;
 
@@ -82,22 +81,30 @@ const ResponseCheck = () => {
       setState('waiting');
       setMessage('너무 성급하시네요 ㅡ.ㅡ 초록색이 된 후에 클릭하세요!');
     } else if (state === 'now') { // 반응속도 체크
-      endTime.current = new Date();
-      setState('waiting');
-      // eslint-disable-next-line react/jsx-key
-      setMessage(['기회는 5번! 다음 화면에서 배경화면이 초록색이 되는 순간 클릭하세요', <br/>, <br/>, '시작하려면 클릭해주세요.']);
-      setResult((prevResult) => {
-        return [...prevResult, endTime.current - startTime.current];
-      });
-    }
-    if(result.length === 5){
-      setState('finish');
-      setMessage('다시하시려면 하단의 다시 버튼을 눌러주세요');
-      e.preventDefault();
+        if(result.length !== 4) {
+          endTime.current = new Date();
+          setState('waiting');
+          // eslint-disable-next-line react/jsx-key
+          setMessage(['기회는 5번! 다음 화면에서 배경이 초록색이 되는 순간 클릭하세요.', <br/>, <br/>, '시작하려면 클릭해주세요.']);
+          setResult((prevResult) => {
+            return [...prevResult, endTime.current - startTime.current];
+          });
+      } else {
+        endTime.current = new Date();
+        setResult((prevResult) => {
+          return [...prevResult, endTime.current - startTime.current];
+        });
+        setState('finish');
+        setMessage('게임이 종료됐습니다. 게임을 다시하려면 아래 다시 버튼을 눌러주세요!');
+        e.preventDefault();
+      }
     }
   };
   const onReset = () => {
     setResult([]);
+    setState('waiting');
+    // eslint-disable-next-line react/jsx-key
+    setMessage(['기회는 5번! 다음 화면에서 배경이 초록색이 되는 순간 클릭하세요.', <br/>, <br/>, '시작하려면 클릭해주세요.']);
   };
   
   const renderAverage = () => {
@@ -106,21 +113,33 @@ const ResponseCheck = () => {
       : result.length === 5
       ? 
       <>
-        <TimeWrapper>
-          <Li>평균 : {Math.round(result.reduce((a, c) => a + c) / result.length)}ms</Li>
-          <Li>{Math.round(result.reduce((a, c) => a + c) / result.length) > 250 ? '연습 좀 더 하셔야겠네요!' : Math.round(result.reduce((a, c) => a + c) / result.length) > 220 ? '이 정도면 전체 평균 정도 수준이네요!' : '정말 빠른 반응 속도를 가지고 있네요!'}</Li>
-        </TimeWrapper>
+        <Ul>
+          <Li>
+            평균 : {Math.round(result.reduce((a, c) => a + c) / result.length)}ms
+          </Li>
+          <Li>
+            {Math.round(result.reduce((a, c) => a + c) / result.length) > 250 
+            ?
+            '연습 좀 더 하셔야겠네요!' 
+            : 
+            Math.round(result.reduce((a, c) => a + c) / result.length) > 220 
+            ? 
+            '이 정도면 전체 평균 정도 수준이네요!' 
+            : 
+            '정말 빠른 반응 속도를 가지고 있네요!'}
+          </Li>
+        </Ul>
         <ButtonWrapper onClick={onReset}>다시!</ButtonWrapper>
       </>
       :
        <>
-        <TimeWrapper>
+        <Ul>
           <li>1회차 : {result[0] ? `${result[0]}ms` : null}</li>
           <li>2회차 : {result[1] ? `${result[1]}ms` : null}</li>
           <li>3회차 : {result[2] ? `${result[2]}ms` : null}</li>
           <li>4회차 : {result[3] ? `${result[3]}ms` : null}</li>
           <li>5회차 : {result[4] ? `${result[4]}ms` : null}</li>
-        </TimeWrapper>
+        </Ul>
         
       </>
   };
