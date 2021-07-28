e835f7a66cd1177a9f6714e7f4adefbf05dbd2ac
TAEWOONG KANG <ktw2378@korea.ac.kr>
2021-07-28 10:18:18 +0900

    서버 구축 파일 추가

--- back/app.js
+++ back/app.js
@@ -3,9 +3,7 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
-const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
-const passport = require('passport');
 const helmet = require('helmet');
 const hpp = require('hpp');
 const redis = require('redis');
@@ -22,26 +20,14 @@ const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
 const likeRouter = require('./routes/like');
 const { sequelize } = require('./models');
-const passportConfig = require('./passport');
 const logger = require('./logger');
 
 const app = express();
-passportConfig(); // 패스포트 설정
 // 개발할 때 포트와 배포할 때 포트를 다르게 한다
 // 나중에는 .env에 PORT=80을 넣어줄 것.
 app.set('port', process.env.PORT || 8001);
-// 넌적스 설정
-app.set('view engine', 'html');
-nunjucks.configure('views', {
-  express: app,
-  watch: true,
-});
+
 // 시퀄라이즈 설정
-// force: true를 하면 테이블이 지워졌다가 다시 생성된다. 대신에 이렇게하면 데이터가 날아가기 때문에 주의해야한다.
-
-// alter: true를 하면 데이터를 유지하면서 테이블을 바꿔주는 대신 컬럼과 데이터가 안 맞아 오류가 나는 경우가 많다.
-
-// 기본적으로 개발용일 때는 force: false를 해놨다가 만약 테이블을 수정하면 force: true를 해줘서 테이블을 재생성해준 뒤 다시 force: false로 바꿔주기. 실무에서는 보통 alter: true를 사용해준다.
 sequelize.sync({ force: false })
   .then(() => {
     console.log('데이터베이스 연결 성공');
@@ -78,9 +64,6 @@ if(process.env.NODE_ENV === 'production') {
 
 app.use(session(sessionOption));
 // express-session보다 아래에 위치해야됨. session을 받아서 실행해야하기 때문에.
-// passport.session이 실행될 때, index.js의 deserializeUser가 실행된다
-app.use(passport.initialize());   // 요청(req 객체)에 passport 설정을 심는다.
-app.use(passport.session());      // req.session 객체에 passport 정보를 저장한다. 따라서 express-session 뒤에서 실행
 
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
@@ -100,7 +83,7 @@ app.use((req, res, next) => {
 // 에러 처리 미들웨어
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  // 개발 모드일 떄는 에러를 보여주게 하고, 배포일 때는 보여주지 않게 하는 코드
+  // 개발 모드일 때는 에러를 보여주게 하고, 배포일 때는 보여주지 않게 하는 코드
   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500);
   res.render('error');
